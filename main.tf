ca07e75772f09ef314dfe18e37bc66a989366b55
manasianu <111309945+manasianu@users.noreply.github.com>
2022-08-21 00:37:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,12 @@ terraform {
    }
  }
  provider "azurerm" {
-   features {}
+  features {}
+  subscription_id = "2134d62e-5ae7-49a1-9bd5-c793f3b0a74c"
+  client_id       = "eeea62ec-cc64-4e2a-867e-76554e57a839"
+  client_secret   = "xmv8Q~X-AhW5ilIE.y9Xt9aWGqmkiM1ogDzClaLK"
+  tenant_id       = "33bc5ab6-4632-439e-8d43-201b74aaa8b3"
+
  }
 
 resource "azurerm_resource_group" "example" {
